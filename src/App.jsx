b366066bf58d4cbd70fd35361c4e977a35184404
xSantanxx <dmitripower@yahoo.com>
2025-07-26 20:39:46 -0400

    created a pop up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,79 @@
+import React from "react";
+import { useState } from "react";
+import ReactDom from "react-dom";
+
+function popUpFadeOut(){
+  const popUp = document.getElementById("signUpSheet");
+  const button = document.getElementById("signInButton");
+}
+
+// var changeFade = React.createClass({
+//   onClick: function(event){
+//     setFade(true);
+//     setFade(false);
+//   }
+// })
+
+
+
 function App() {
+
+  const [fadeIn, setFade] = useState(false);
+  // const []
 
   return (
 
-      <div className="h-screen w-screen bg-indigo-950 grid justify-items-center">
-        <div className="relative flex size-3 bg-green-500 mt-6 h-20 w-20 rounded-[10vw] flex items-center justify-center">
-          
-          <button type="submit" className="outline-3 hover:outline-sky-400 bg-tranparent px-5 py-5 h-20 w-20 rounded-[10vw] cursor-pointer 
-          ">
+      <div className="relative h-screen w-screen bg-indigo-950 grid justify-items-center">
+        <div className={`fixed bg-green-500 left-[50%] mt-6 h-20 w-20 rounded-[10vw] flex 
+          items-center justify-center outline-3 hover:outline-sky-400 animate-bounce
+          top-[25%] transition-all duration-500 ease-out
+          ${fadeIn ? "opacity-0" : "opacity-100"}`}>
+            <button disabled={fadeIn} type="button" onClick={() => {
+              fadeIn ? setFade(false) : setFade(true);
+            }}
+          className="bg-tranparent px-5 py-5  
+          rounded-[10vw] cursor-pointer 
+          text-7xl opacity-100">
             !</button>
-            {/* <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 z-0"></span> */}
-        </div>
+          </div>
+        {/* <div className="relative bg-green-500 
+        mt-6 h-20 w-20 rounded-[10vw] 
+        flex items-center justify-center
+        outline-3 hover:outline-sky-400
+        animate-bounce
+        top-[25%]"> */}
+        {/* </div> */}
+        <div className={`border-2 border-solid fixed h-1/2 w-2xl left-[20%] top-[33%] rounded-2xl bg-rose-200
+          transition-all duration-500 ease-in justify-items-center z-40
+          ${fadeIn ? "opacity-100" : "opacity-0"}`}>
+            <div className="bg-pink-50 rounded-t-lg justify-items-start grid
+            min-h-6 relative">
+              <button type="button" onClick={() => setFade(false)} className="bg-red-800 rounded-full w-4 h-4
+              hover:animate-pulse absolute left-0.5 top-1 cursor-pointer
+              p-2 flex items-center justify-center text-xs
+              font-thin text-red-50"
+              ><p className="opacity-90 hover:opacity-100 duration-500 text-gray-600/100 
+              text-lg">x</p></button>
+              <button className="bg-amber-500 w-4 h-4 absolute left-5 top-1
+              hover:animate-pulse rounded-full"></button>
+              <button className="bg-green-500 w-4 h-4 absolute left-9.5 top-1
+              hover:animate-pulse rounded-full"></button>
+            </div>
+            <h3 className="leading-relaxed text-2xl font-mon relative left-5 top-2
+            text-black">Sign in our platform</h3>
+            <div className="relative bg-slate-400/20 w-auto rounded-full
+            h-1 top-5 mx-4"></div>
+            <div className="relative top-10 mx-5 grid grid-cols-1 gap-2">
+              <label htmlFor="email">Your Email</label>
+              <input type="email" placeholder="steve@gmail.com" name="email" className="outline-2 rounded-md
+              w-[50%]"/>
+            </div>
+            <div className="relative top-[15%] mx-5 grid grid-cols-1 gap-2 z-40">
+              <label htmlFor="password">Your Password</label>
+              <input type="password" placeholder="●●●●●●●" name="password" className="outline-2 rounded-md
+              w-[50%]"/>
+            </div>
+          </div>
       </div>
   )
 }
